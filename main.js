c904018a0d0bf686f975c250d877cfee68142da6
nssuperx <mgtmgtx@gmail.com>
2020-08-17 14:15:21 +0900

    行選択ができるようになった。

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ class sentence{
     constructor(str, lineNum){
         this.sentenceStr = str;
         this.lineNum = lineNum;
+        this.id = 'line' + lineNum.toString();
         this.classes = new Set();
+        this.classes.add('line');
     }
 
     addClass(str){
@@ -25,12 +27,13 @@ class sentence{
      * NOTE: addEventListenerの使い方がよくわからなかったため、無理やりこのようにしている。
      */
     static select(){
-        console.log(this._this);
+        // console.log(this);
         if(this._this.classes.has('selected')){
             this._this.deleteClass('selected');
         }else{
             this._this.addClass('selected');
         }
+        drawLine(this._this);
     }
 }
 
@@ -44,7 +47,6 @@ var sentences = new Array();
 function loadOriginal(input){
     let file = input.files[0];
     let reader = new FileReader();
-    let idElement = document.getElementById('sentenceParent');
     reader.readAsText(file);
     reader.onload = () => {
         let lines = splitText(reader.result)
@@ -53,20 +55,44 @@ function loadOriginal(input){
             sentences.push(new sentence(lines[i], i));          //文字列クラスを文章に入れていく
         }
         // console.log(sentences);
-        let table = document.createElement('table')             //テーブル作成
-        for(let s of sentences){                              //作ったsentencesから一文ずつ取り出してテーブルを作る
-            let tr = table.insertRow();                         //trタグ追加
-            tr.id = 'line' + s.lineNum.toString();
-            tr.className = 'line';
-            tr.addEventListener('click', {_this: s, handleEvent: sentence.select}, false);
-            let td = tr.insertCell();                           //tdタグ追加
-            td.classList.add('lineNum');
-            td.innerHTML = s.lineNum;
-            td = tr.insertCell();                               //tdタグ追加
-            td.innerHTML = s.sentenceStr;
-        }
-        idElement.appendChild(table);                           //作ったテーブルを子要素として追加
+        drawText();
     };
+}
+
+/**
+ * 一文（一行）だけ描画しなおす。
+ * @param {sentence} s  一文
+ */
+function drawLine(s){
+    // console.log(s);
+    let idElement = document.getElementById(s.id);
+    idElement.className = null;
+    for(let c of s.classes){
+        idElement.classList.add(c);
+    }
+}
+
+/**
+ * 文章全体を描画する。
+ */
+function drawText(){
+    let idElement = document.getElementById('sentenceParent');
+    idElement.textContent = null;
+    let table = document.createElement('table')             //テーブル作成
+    for(let s of sentences){                                //作ったsentencesから一文ずつ取り出してテーブルを作る
+        let tr = table.insertRow();                         //trタグ追加
+        tr.id = 'line' + s.lineNum.toString();
+        for(let c of s.classes){
+            tr.classList.add(c);
+        }
+        tr.addEventListener('click', {_this: s, handleEvent: sentence.select}, false);
+        let td = tr.insertCell();                           //tdタグ追加
+        td.classList.add('lineNum');
+        td.innerHTML = s.lineNum;
+        td = tr.insertCell();                               //tdタグ追加
+        td.innerHTML = s.sentenceStr;
+    }
+    idElement.appendChild(table);                           //作ったテーブルを子要素として追加
 }
 
 /**
