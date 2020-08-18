4118498bbb8951b2e76e30192b9a97249b68b7b9
nssuperx <mgtmgtx@gmail.com>
2020-08-18 14:02:20 +0900

    リファクタリングとコメント

--- main.js
+++ main.js
@@ -1,104 +1,31 @@
 /**
- * 一文クラス
- */
-class sentence{
-    /**
-     * @param {string} str 一文
-     * @param {number} lineNum 行番号
-     */
-    constructor(str, lineNum){
-        this.sentenceStr = str;
-        this.lineNum = lineNum;
-        this.id = 'line' + lineNum.toString();
-        this.classes = new Set();
-        this.classes.add('line');
-    }
-
-    addClass(str){
-        this.classes.add(str);
-    }
-
-    deleteClass(str){
-        this.classes.delete(str);
-    }
-
-    /**
-     * 選択してるかしてないかクラスを設定。
-     * NOTE: addEventListenerの使い方がよくわからなかったため、無理やりこのようにしている。
-     */
-    static select(){
-        // console.log(this);
-        if(this._this.classes.has('selected')){
-            this._this.deleteClass('selected');
-        }else{
-            this._this.addClass('selected');
-        }
-        drawLine(this._this);
-    }
-}
-
-function toggleLineSelect(){
-    console.log(this);
-    this.classList.toggle('selected');
-}
-
-/** 文章 一文の配列*/
-var sentences = new Array();
-
-/**
  * 生テキストファイルを読み込む。
  * @param input onchangeのときのthis
  */
 function loadOriginal(input){
     let file = input.files[0];
     let reader = new FileReader();
+    let idElement = document.getElementById('sentenceParent');
+    idElement.textContent = null;
+    let table = document.createElement('table')                         //テーブル作成
+    table.id = 'lines';
     reader.readAsText(file);
     reader.onload = () => {
         let lines = splitText(reader.result)
         let linesNum = lines.length;
-        for(let i=0; i<linesNum; i++){
-            sentences.push(new sentence(lines[i], i));          //文字列クラスを文章に入れていく
+        for(let i=0; i<linesNum; i++){                                  //作ったsentencesから一文ずつ取り出してテーブルを作る
+            let tr = table.insertRow();                                 //trタグ追加
+            tr.id = 'line' + i.toString();
+            tr.classList.add('line');
+            tr.addEventListener('click', toggleLineSelect, false);
+            let td = tr.insertCell();                                   //tdタグ追加
+            td.classList.add('lineNum');
+            td.innerHTML = i.toString();
+            td = tr.insertCell();                                       //tdタグ追加
+            td.innerHTML = lines[i];
         }
-        // console.log(sentences);
-        drawText();
+        idElement.appendChild(table);                                   //作ったテーブルを子要素として追加
     };
-}
-
-/**
- * 一文（一行）だけ描画しなおす。
- * @param {sentence} s  一文
- */
-function drawLine(s){
-    // console.log(s);
-    let idElement = document.getElementById(s.id);
-    idElement.className = "";
-    for(let c of s.classes){
-        idElement.classList.add(c);
-    }
-}
-
-/**
- * 文章全体を描画する。
- */
-function drawText(){
-    let idElement = document.getElementById('sentenceParent');
-    idElement.textContent = null;
-    let table = document.createElement('table')             //テーブル作成
-    table.id = 'lines';
-    for(let s of sentences){                                //作ったsentencesから一文ずつ取り出してテーブルを作る
-        let tr = table.insertRow();                         //trタグ追加
-        tr.id = 'line' + s.lineNum.toString();
-        for(let c of s.classes){
-            tr.classList.add(c);
-        }
-        tr.addEventListener('click', {_this: s, handleEvent: sentence.select}, false);
-        let td = tr.insertCell();                           //tdタグ追加
-        td.classList.add('lineNum');
-        td.innerHTML = s.lineNum;
-        td = tr.insertCell();                               //tdタグ追加
-        td.innerHTML = s.sentenceStr;
-    }
-    idElement.appendChild(table);                           //作ったテーブルを子要素として追加
 }
 
 /**
@@ -112,21 +39,9 @@ function splitText(rawtext){
 }
 
 /**
- * 行番号を右揃えにするために気合でスペースを入れる。使ってない。
- * @param {number} num 行番号
- * @param {number} linesNum 行数
- * @return {string} スペース入り行番号文字列
+ * jsonファイルを読み込む
+ * @param input onchangeのときのthis
  */
-function makeLineNumber(num, linesNum){
-    let digits = linesNum.toString().length;
-    let numstr = "";
-    for(let i=num.toString().length; i<digits; i++){
-        numstr += '&nbsp';
-    }
-    numstr += num.toString();
-    return numstr;
-}
-
 function inputJson(input){
     let file = input.files[0];
     let reader = new FileReader();
@@ -135,20 +50,20 @@ function inputJson(input){
         let text = JSON.parse(reader.result);
         let idElement = document.getElementById('sentenceParent');
         idElement.textContent = null;
-        let table = document.createElement('table')             //テーブル作成
+        let table = document.createElement('table')                     //テーブル作成
         table.id = 'lines';
         for(let t of text){
-            let tr = table.insertRow();                         //trタグ追加
+            let tr = table.insertRow();                                 //trタグ追加
             tr.id = 'line' + t[0][1].toString();
             tr.className = t[1][1];
             tr.addEventListener('click', toggleLineSelect, false);
-            let td = tr.insertCell();                           //tdタグ追加
+            let td = tr.insertCell();                                   //tdタグ追加
             td.classList.add('lineNum');
             td.innerHTML = t[0][1].toString();
-            td = tr.insertCell();                               //tdタグ追加
+            td = tr.insertCell();                                       //tdタグ追加
             td.innerHTML = t[2][1];
         }
-        idElement.appendChild(table);                           //作ったテーブルを子要素として追加
+        idElement.appendChild(table);                                   //作ったテーブルを子要素として追加
     };
 }
 
@@ -168,26 +83,11 @@ function outputJson(){
     let downloadData = JSON.stringify([...out]);
     let blob = new Blob([downloadData], { "type" : "text/plain" });
     document.getElementById("output").href = window.URL.createObjectURL(blob);
-    console.log(downloadData);
 }
 
-
 /**
- * Mapを気合でStringにする。使ってない。
- * WARNING: 文字列中にダブルクオートとかあったら使えない。
- * @param {Map} map 
- * @return String
+ * 選択されてるかされてないかクラスを設定
  */
-function mapToString(map){
-    let str = new String();
-    str += "{";
-    for(let [key, value] of map){
-        str += "\"" + String(key) + "\"";
-        str += ":";
-        str += "\"" + String(value);
-        str += ","
-    }
-    str = str.slice(0, -1);
-    str += "}";
-    return str;
+function toggleLineSelect(){
+    this.classList.toggle('selected');
 }
